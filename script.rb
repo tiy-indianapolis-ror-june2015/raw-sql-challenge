b979df771dfcb3dc28a5aa6285c6231e8fe97095
James Dabbs <jamesdabbs@gmail.com>
2015-06-02 09:36:45 -0400

    Add solution

--- script.rb
+++ script.rb
@@ -1,15 +1,14 @@
 require 'csv'
 
 def command_to_add_user first_name, last_name, email
-  "SELECT COUNT(*) FROM users;" # Write the correct SQL statement here
+  # "SELECT COUNT(*) FROM users;" # Write the correct SQL statement here
+  "INSERT INTO users VALUES (null,'#{first_name}','#{last_name}','#{email}');"
 end
 
 def run_command sql
   system %{ sqlite3 db.sqlite3 \"#{sql}\" }
 end
 
-File.open "commands", "w" do |f|
-  CSV.foreach "new_users.csv" do |first_name, last_name, email|
-    run_command command_to_add_user(first_name, last_name, email)
-  end
+CSV.foreach "new_users.csv" do |first_name, last_name, email|
+  run_command command_to_add_user(first_name, last_name, email)
 end
